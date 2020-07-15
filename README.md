2f41f62da57de1d72534a377e8db53a20763ce28
madderlake <mary@maryahayne.com>
2020-07-15 09:13:52 -0700

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+### Javascript Algorithms and Exercises
+
+This is a collection of problems that I have collected over the past couple of years.
--- countLetters.js
+++ countLetters.js
@@ -1,30 +1,28 @@
 function countLtrs(str) {
-  var counts = {};
-  str = str.replace(/[^A-Za-z]/g, '');
-  console.log(str);
-  // Misc vars
-  var ch, i, count;
+    var counts = {};
+    str = str.replace(/[^A-Za-z]/g, "");
+    console.log(str);
+    // Misc vars
+    var ch, i, count;
 
-  // Loop through the string...
-  for (i = 0; i < str.length; ++i) {
-    // Get this character
-    ch = str.charAt(i); // Not all engines support [] on strings
+    // Loop through the string...
+    for (i = 0; i < str.length; ++i) {
+        // Get this character
+        ch = str.charAt(i); // Not all engines support [] on strings
 
-    // Get the count for it, if we have one; we'll get `undefined` if we
-    // don't know this character yet
-    count = counts[ch];
+        // Get the count for it, if we have one; we'll get `undefined` if we
+        // don't know this character yet
+        count = counts[ch];
 
-    // If we have one, store that count plus one; if not, store one
-    // We can rely on `count` being falsey if we haven't seen it before,
-    // because we never store falsey numbers in the `counts` object.
-    counts[ch] = count ? count + 1 : 1;
-  }
-  console.log(counts);
-  return counts;
+        // If we have one, store that count plus one; if not, store one
+        // We can rely on `count` being falsey if we haven't seen it before,
+        // because we never store falsey numbers in the `counts` object.
+        counts[ch] = count ? count + 1 : 1;
+    }
+    console.log(counts);
+    return counts;
 }
 
-console.log(
-  countLtrs(
-    'I want to count the number of occurances of each char in this string'
-  )
-);
+console.log(countLtrs(
+    "I want to count the number of occurances of each char in this string"
+));
